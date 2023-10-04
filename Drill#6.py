b60e2eb86e835e4afff8c40fb39a9616fa11864c
chalbangE <yubin41029@naver.com>
2023-10-04 20:22:27 +0900

    Reformat

--- Drill#6.py
+++ Drill#6.py
@@ -27,6 +27,7 @@ def handle_events():
             running = False
     pass
 
+
 def reset_world():
     global running, cx, cy, frame, mx, my, t, action, points
 
@@ -38,10 +39,11 @@ def reset_world():
     points = []
     set_new_target_arrow()
 
+
 def set_new_target_arrow():
     global sx, sy, hx, hy, t, action, frame, target_exists
 
-    if points: # points 리스트안에 남아있는 점이 있으면,
+    if points:  # points 리스트안에 남아있는 점이 있으면,
         sx, sy = cx, cy  # p1 : 시작점
         # hx, hy = 50, 50
         hx, hy = points[0]  # p2 : 끝점.
@@ -50,9 +52,10 @@ def set_new_target_arrow():
         frame = 0
         target_exists = True
     else:
-        action = 3 if action == 1 else 2   # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
+        action = 3 if action == 1 else 2  # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
         frame = 0
         target_exists = False
+
 
 def render_world():
     clear_canvas()
@@ -76,12 +79,13 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx 는 시작 x 와 끌 x 를 1-t:t 의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else: # 목표지점에 도달하면
-            cx, cy = hx, hy # 캐릭터 위치를 기존 목적지 위치와  강제로 정확히 일치시킴.
-            del points[0] # 목표지점에 왔기 때문에, 더 이상 필요없는 점을 삭제
+        else:  # 목표지점에 도달하면
+            cx, cy = hx, hy  # 캐릭터 위치를 기존 목적지 위치와  강제로 정확히 일치시킴.
+            del points[0]  # 목표지점에 왔기 때문에, 더 이상 필요없는 점을 삭제
             set_new_target_arrow()
-    elif points: # 목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
+    elif points:  # 목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
         set_new_target_arrow()
+
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
