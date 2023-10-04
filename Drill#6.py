6a941b3f9212ad306165f50997fde613cccda050
chalbangE <yubin41029@naver.com>
2023-10-04 20:16:53 +0900

    손 위치 리스트를 만들고, 모든 손들을 그림.

--- Drill#6.py
+++ Drill#6.py
@@ -14,25 +14,26 @@ def load_resources():
 
 
 def handle_events():
-    global running
+    global running, mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-
+        elif event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
 def reset_world():
-    global running, cx, cy, frame
-    global t
-    global action
+    global running, cx, cy, frame, mx, my, t, action, points
 
+    mx, my = 0, 0
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
     action = 3
+    points = [(100, 900), (1200, 800), (500, 100)]
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t
@@ -48,6 +49,9 @@ def set_new_target_arrow():
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    for p in points:
+        arrow.draw(p[0], p[1])
+    arrow.draw(mx, my)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
